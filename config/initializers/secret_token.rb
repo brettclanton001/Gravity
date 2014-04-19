39f751a8a0369e0c0e2595b9973f9bf44e05389f
brettclanton001 <brettclanton001@gmail.com>
2014-04-19 12:56:30 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gravity::Application.config.secret_key_base = 'bbe88853849e28d00df91da14fc83ead29d653f2b39d03a0f773276f5e015acccfeb222e32cc4e352c75e2e1a2eaf2a6782c4f7815b8eac3ea4da8a16a679f09'
