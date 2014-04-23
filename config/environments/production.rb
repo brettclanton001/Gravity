4fb86cdf648965aca2d5e0b613591d85f1b66757
brettclanton001 <brettclanton001@gmail.com>
2014-04-22 22:24:32 -0400

    prod email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,18 @@ Gravity::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'powerful-sea-1103.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => "brettclanton001@gmail.com",
+    :password => ENV['GMAIL_PASSWORD']
+  }
+
 end
