7fd37663c85e8fa12ba4dc7c0518a517782e6cee
brettclanton001 <brettclanton001@gmail.com>
2014-06-03 20:54:33 -0400

    allow 100% discounted users to skip payment methods

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -3,7 +3,9 @@ class AccountController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def uploads
-    redirect_to account_payment_methods_path, alert: 'You must have an active payment method on file to use this application.' unless current_user.active_payments
+    if !current_user.active_payments && current_user.discount_percent < 100
+      redirect_to account_payment_methods_path, alert: 'You must have an active payment method on file to use this application.'
+    end
   end
 
   def payment_methods
--- spec/features/account_spec.rb
+++ spec/features/account_spec.rb
@@ -32,4 +32,31 @@ describe "Account/Stripe functionality" do
     end
   end
 
+  describe "Uploads Page" do
+    it "should redirect users to payment methods page when the user has no payment methods" do
+      visit uploads_path
+      current_url.include?('/account/payment_methods').should be true
+    end
+    it "should redirect users to payment methods page when the user has no payment methods and no discount" do
+      @user.update_attribute(:discount_percent, 0)
+      visit uploads_path
+      current_url.include?('/account/payment_methods').should be true
+    end
+    it "should redirect users to payment methods page when the user has no payment methods and some discount" do
+      @user.update_attribute(:discount_percent, 50)
+      visit uploads_path
+      current_url.include?('/account/payment_methods').should be true
+    end
+    it "should not redirect users to payment methods page when the user has no payment methods and 100% discount" do
+      @user.update_attribute(:discount_percent, 100)
+      visit uploads_path
+      current_url.include?('/uploads').should be true
+    end
+    it "should keep users at the uploads page when the user has a payment method" do
+      @user.update_attribute(:active_payments, true)
+      visit uploads_path
+      current_url.include?('/uploads').should be true
+    end
+  end
+
 end
